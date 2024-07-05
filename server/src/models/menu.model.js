c74df2aec2f71ed578276537f4106d31b23f6f6f
Leonardo Balbastro <leonardonbalbastro@gmail.com>
2024-07-05 11:46:22 -0300

    FEAT: definicion de esquema del modelo de menu

--- server/src/models/menu.model.js
+++ server/src/models/menu.model.js
@@ -1,4 +1,32 @@
 //Modelo de el menu
+import mongoose from 'mongoose'
+
+const menuSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    ingredients: [],
+    estimatedTimeToDeliver: {
+        type: Number
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    available: {
+        type: Boolean,
+        required: true,
+    }
+})
+
+const MenuModel = mongoose.model('Menu', menuSchema)
+
+export default MenuModel
 
 //Nombre del menu
 //Descripcion del menu
