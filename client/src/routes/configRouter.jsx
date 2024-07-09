b931a427a3d0103189ad09c399ff27392d3dfdb6
Sofia <sperezhidd@gmail.com>
2024-07-09 10:48:44 -0400

    Crear y añadir ruta login

--- client/src/routes/configRouter.jsx
+++ client/src/routes/configRouter.jsx
@@ -1,12 +1,17 @@
 import Home from '../views/Home/Home.jsx'
+import Login from '../views/Login/Login.jsx'
 import NotFound from '../views/NotFound/NotFound.jsx'
 
 const routerPathComponent = [
 	{
-		path: '/home',
+		path: '/',
 		element: <Home />,
 	},
 	{
+		path: '/login', 
+		element: <Login />
+	}
+	,{
 		path: '*',
 		element: <NotFound />,
 	},
---
+++ client/src/views/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignIn from '../../components/SignIn'
+
+export default function Login() {
+  return (
+    <div>
+        <SignIn />
+    </div>
+  )
+}
