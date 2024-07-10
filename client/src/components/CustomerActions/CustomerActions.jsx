5676c39c8574c9ebe3c2508b1b8319930da28575
Paolo Suarez <ptsuarez91@hotmail.com>
2024-07-10 16:06:45 -0400

    feat: Agregado el componente CustomerActions y NavBar con fondo dinámico
    - Se dio estilos a CustomerActions para mostrar las acciones principales del cliente.
    - Integrado el componente NavBar con fondo dinámico basado en props.
    - Definido el objeto bgStyles para alternar entre bg-customLight y bg-customBlue.
    - Actualizada la configuración de Tailwind CSS para incluir colores personalizados.
    - Se agregaron íconos (arrowLeftImg, waiterImg, questionImg) para NavBar.
    Estos cambios incluyen mejoras en la interacción del cliente y elementos visuales.

--- client/src/components/CustomerActions/CustomerActions.jsx
+++ client/src/components/CustomerActions/CustomerActions.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { callWaiter, showMenu } from "../../utils/functions/customerActions";
+import NavBar from "../NavBar/NavBar";
 
 const CustomerActions = () => {
   return (
-    <div className="flex flex-col items-center justify-around min-h-screen">
-      <h2 className="text-xl font-bold m-10">Bienvenido</h2>
-      <div className="flex flex-col items-center justify-center p-6">
-        <button className="bg-amber-600 p-3 rounded-xl text-white w-40" onClick={callWaiter}>Llamar al mesero</button>
-        <button className="bg-amber-600 p-3 rounded-xl text-white my-4 w-40" onClick={showMenu}>Mostrar el menú</button>
+    <div className="flex flex-col items-center justify-around min-h-screen bg-customLight">
+    <NavBar bgMain={"bg-customLight"}/>
+      <h2 className="text-4xl font-bold m-10">Bienvenidos</h2>
+      <div className="flex flex-col grow gap-[10px] text-2xl leading-7 font-bold  min-w-[369px] md:min-w-[419px] md:justify-center lg:min-w-[469px]">
+        <button className="bg-customBlue p-[10px] rounded-[20px] h-[55px] text-white  my-4 w-full" onClick={showMenu}>CONSULTAR MENÚ</button>
+        <button className="bg-customBlue p-[10px]  rounded-[20px] h-[55px] text-white w-full" onClick={callWaiter}>LLAMAR AL MOZO</button>
+        <button className="bg-customBlue p-[10px]  rounded-[20px] h-[55px] text-white my-4 w-full" onClick={showMenu}>RESERVAS</button>
       </div>
       
     </div>
