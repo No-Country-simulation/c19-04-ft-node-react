c034f801a04a42434ff155fe46ee0c41aff903bd
Sofia <sperezhidd@gmail.com>
2024-07-10 13:26:02 -0400

    feat: agregar validación para prevenir espacios en blanco en inputs

--- client/src/utils/functions/handlerChangeForm.js
+++ client/src/utils/functions/handlerChangeForm.js
@@ -1,22 +1,26 @@
-import { validateForm } from "./validateForm";
+import { validateForm } from './validateForm';
 
 export function handleChangeForm(setFormData) {
   return (event) => {
+    const valueWithoutSpaces = event.target.value.replace(/\s/g, '');
+
     setFormData((prevFormData) => ({
       ...prevFormData,
-      [event.target.name]: event.target.value,
+      [event.target.name]: valueWithoutSpaces,
     }));
   };
 }
 
 export function handlerSubmitRegister(formData, setErrors) {
+  console.log(formData);
   return (event) => {
     event.preventDefault();
     const newErrors = validateForm(formData);
     setErrors(newErrors);
 
     if (Object.keys(newErrors).length === 0) {
-      console.log("Formulario enviado:", formData);
+      console.log('Formulario enviado:', formData);
     }
   };
 }
+
