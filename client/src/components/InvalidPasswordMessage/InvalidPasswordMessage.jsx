a888c95b7ee651777ad49ce5acd33a43bfbe9047
Rocio Tellez Lopez <rociotellezlopez@gmail.com>
2024-07-11 08:15:17 -0500

    feat: Implementa countdown.js para agregar conteo en el componente MessageRedirect y modifica estilos al componente.

--- client/src/components/InvalidPasswordMessage/InvalidPasswordMessage.jsx
+++ client/src/components/InvalidPasswordMessage/InvalidPasswordMessage.jsx
@@ -4,14 +4,16 @@ import MessageRedirect from "../MessageRedirect/MessageRedirect";
 
 const InvalidPasswordMessage = () => {
   const messageInvalid = {
-    title: "Invalid Password",
-    message: "The password you entered is invalid. Please try again.",
+    title: "Contraseña Invalida",
+    message: "Lo sentimos, la contraseña que has ingresado no es correcta.",
+    path: "/login"
   };
   return (
     <div>
       <MessageRedirect
         title={messageInvalid.title}
         message={messageInvalid.message}
+        path={messageInvalid.path}
       />
     </div>
   );
