cb6d51a467f36c43a1ef5851e0711164e568ec3c
Martin <martin_08_alba@hotmail.com>
2024-07-23 18:02:39 +0200

    feat: (script) verifyAdmin & linter: ./server/

--- server/src/connections/server.js
+++ server/src/connections/server.js
@@ -1,5 +1,6 @@
 import dbConnection from './mongoose.js'
 import logger from '../utils/logger.js'
+import { verifyAdmin } from '../utils/verifyAdmin.js'
 
 export const ServerUp = async (app) => {
 	try {
@@ -7,6 +8,7 @@ export const ServerUp = async (app) => {
 		app.listen(process.env.PORT, () => {
 			logger.info(`Server running on port ${process.env.PORT}`)
 		})
+		verifyAdmin()
 	} catch (err) {
 		logger.fatal(`Error starting server: ${err}`)
 	}
--- server/src/controllers/waiter.controller.js
+++ server/src/controllers/waiter.controller.js
@@ -1,10 +1,15 @@
-import { sendMessage, assignTables, getRequestedTables, deleteRequestTable } from '../services/waiter.service.js'
+import {
+	sendMessage,
+	assignTables,
+	getRequestedTables,
+	deleteRequestTable,
+} from '../services/waiter.service.js'
 
 const WaiterController = {
-    sendMessage,
-    assignTables,
-    getRequestedTables,
-    deleteRequestTable,
+	sendMessage,
+	assignTables,
+	getRequestedTables,
+	deleteRequestTable,
 }
 
 export default WaiterController
