485e11de2ed62119b6b0ea3b9a2c29758b65a5bb
Thiago <thiagosalaberry99@gmail.com>
2024-07-25 23:27:45 -0300

    FEAT-Update order status

--- server/src/services/order.service.js
+++ server/src/services/order.service.js
@@ -4,10 +4,10 @@ import logger from "../utils/logger.js";
 import fs from "fs";
 import path from "path";
 import { ordersRef } from "../utils/firebaseRefs.js";
-import { get, set } from "firebase/database";
+import { get, set, ref } from "firebase/database";
 import { v4 as uuidv4 } from "uuid";
-import {} from "../controllers/menu.controller.js";
 import MenuController from "../controllers/menu.controller.js";
+import database from "../connections/firebase.js";
 export const getAllOrders = async (req, res) => {
   try {
     const allOrders = await OrderModel.find();
@@ -69,8 +69,11 @@ export const createOrder = async (req, res) => {
   if (!to || !tableNumber || !order)
     return res.status(400).json({ error: "Missing required fields." });
   try {
-    const ordersSnapshot = await get(ordersRef);
-    const ordersData = ordersSnapshot.val();
+    const allOrdersSnapshot = await get(ordersRef);
+    const allOrdersData = allOrdersSnapshot.val();
+    const pendingRef = ref(database, `/orders/${to}/pending`);
+    const pendingSnapshot = await get(pendingRef);
+    const pendingData = pendingSnapshot.val();
     const orderedDishes = await MenuController.getDishesByIds(
       order.map((id) => id)
     );
@@ -83,15 +86,12 @@ export const createOrder = async (req, res) => {
       };
     });
     const orderId = uuidv4();
-    await set(ordersRef, {
-      ...ordersData,
-      [to]: {
-        ...ordersData[to],
-        [orderId]: {
-          orderNumber: ordersData.lastOrder + 1,
-          tableNumber,
-          order: finalOrder,
-        },
+    await set(pendingRef, {
+      ...pendingData,
+      [orderId]: {
+        orderNumber: allOrdersData.lastOrder + 1,
+        tableNumber,
+        order: finalOrder,
       },
     });
     logger.info(`Order created successfully`);
@@ -117,26 +117,59 @@ export const getPendingOrders = async (req, res) => {
 };
 
 export const updateStatus = async (req, res) => {
+  const { orderId } = req.params;
+  const { from, updateTo } = req.body;
   try {
-    const { orderId } = req.params;
-    if (!orderId) {
-      logger.error(`Missing required orderId.`);
-      return res.status(400).send("Missing required orderId.");
+    if (!orderId || !updateTo) {
+      logger.error(`Missing required fields.`);
+      return res.status(400).send("Missing required fields.");
     }
-    const orderToUpdate = await OrderModel.findByIdAndUpdate(
-      orderId,
-      {
-        status: "ready",
-        readyAt: new Date(),
-      },
-      { new: true }
+    const initialOrderRef = ref(
+      database,
+      `/orders/${from == "bar" ? "bar" : "kitchen"}/${
+        updateTo == "inProgress" ? "pending" : "inProgress"
+      }`
     );
-    if (!orderToUpdate) {
-      logger.error(`Order ${orderId} not found.`);
-      return res.status(404).send("Order not found.");
+    const finalOrderRef = ref(
+      database,
+      `/orders/${from == "bar" ? "bar" : "kitchen"}/${
+        updateTo == "inProgress" ? "inProgress" : "ready"
+      }`
+    );
+    const initialSnapshot = await get(initialOrderRef);
+    const initialData = initialSnapshot.val();
+    const finalSnapshot = await get(finalOrderRef);
+    const finalData = finalSnapshot.val() || {};
+    const orderExists =
+      initialData && Object.keys(initialData).find((key) => key == orderId);
+    if (!orderExists) {
+      logger.error("The order to update does not exist.");
+      return res
+        .status(404)
+        .json({ error: "The order to update does not exist." });
     }
-    logger.info(`Order ${orderToUpdate} updated successfully.`);
-    return res.status(200).json(orderToUpdate);
+    let newInitialData = {};
+    for (const key in initialData) {
+      if (key == orderId) {
+        finalData[key] = initialData[key];
+      } else {
+        newInitialData = {
+          ...newInitialData,
+          [key]: initialData[key],
+        };
+      }
+    }
+    await set(initialOrderRef, newInitialData);
+    await set(finalOrderRef, {
+      ...finalData,
+      [orderId]: initialData[orderId],
+    });
+    logger.info(
+      `The order status has been successfully updated to ${updateTo}`
+    );
+    return res.status(200).json({
+      message: `The order status has been successfully updated to ${updateTo}`,
+    });
   } catch (error) {
     logger.error(`Error in order.service.updateStatus: ${error}`);
     res.status(500).send("Internal Server Error");
