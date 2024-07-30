73c7ac70865518774b134cf4b028a77a4da8ec2e
Martin <martin_08_alba@hotmail.com>
2024-07-30 18:11:49 +0200

    refactor: (Controllers & Routes) admin & auth & ingredients & menu & qr

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,15 +1,101 @@
-import {
-	createMenu,
-	updateMenu,
-	removeMenu,
-	closeDay,
-} from '../services/admin.service.js'
+import MenuModel from '../models/menu.model.js'
+import logger from '../utils/logger.js'
+import { get, set } from 'firebase/database'
+import { tablesRef, waitersRef, ordersRef } from '../utils/firebaseRefs.js'
 
-const AdminController = {
-	createMenu,
-	updateMenu,
-	removeMenu,
-	closeDay,
+class AdminController {
+	async createMenu(data) {
+		try {
+			if (!data) {
+				logger.error('Data to update can not be empty!')
+				throw new Error('Data to update can not be empty!')
+			}
+
+			await MenuModel.create(data)
+
+			logger.info(`Menu created successfully: ${data.title}`)
+			return { message: `Menu created successfully: ${data.title}` }
+		} catch (err) {
+			logger.error(`Error in AdminController.createMenu: ${err}`)
+		}
+	}
+
+	async updateMenu(id, data) {
+		try {
+			if (!data) {
+				logger.error('Data to update can not be empty!')
+				throw new Error('Data to update can not be empty!')
+			}
+
+			if (!id) {
+				logger.error(`ID ${id} not found`)
+				throw new Error(`ID ${id} not found`)
+			}
+
+			const menu = await MenuModel.findByIdAndUpdate(id, data, {
+				new: true,
+			})
+
+			logger.info(`Menu updated successfully: ${menu.title}`)
+			return { message: `Menu updated successfully: ${menu.title}` }
+		} catch (err) {
+			logger.error(`Error in AdminController.updateMenu: ${err}`)
+		}
+	}
+
+	async removeMenu(id) {
+		try {
+			if (!id) {
+				logger.error(`ID ${id} not found`)
+				throw new Error(`ID ${id} not found`)
+			}
+
+			await MenuModel.findByIdAndDelete(id)
+
+			logger.info('Menu deleted successfully')
+			return { message: 'Menu deleted successfully' }
+		} catch (err) {
+			logger.error(
+				`Error while removing menu AdminController.removeMenu: ${err}`,
+			)
+		}
+	}
+
+	async closeDay() {
+		try {
+			const initialOrdersData = { lastOrder: 0 }
+			const finalTablesSnapshot = await get(tablesRef)
+			const finalWaitersSnapshot = await get(waitersRef)
+			const finalTablesData = finalTablesSnapshot.val() || {}
+			const finalWaitersData = finalWaitersSnapshot.val() || {}
+
+			const initialTablesData = {}
+
+			for (const key in finalTablesData) {
+				if (key !== 'unassignedTables') {
+					initialTablesData[key] = { isActive: false }
+				}
+			}
+
+			const initialWaitersData = {}
+
+			for (const key in finalWaitersData) {
+				initialWaitersData[key] = {
+					assignedTables: '',
+					requestedBy: '',
+				}
+			}
+
+			await set(tablesRef, initialTablesData)
+			await set(ordersRef, initialOrdersData)
+			await set(waitersRef, initialWaitersData)
+
+			logger.info('Day closed successfully')
+			return { message: 'Day closed successfully' }
+		} catch (err) {
+			logger.error(`Error in AdminController.closeDay: ${err}`)
+		}
+	}
 }
 
-export default AdminController
+export default new AdminController()
--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,39 +1,141 @@
 import { Router } from 'express'
+import AdminController from '../controllers/admin.controller.js'
 import AuthController from '../controllers/auth.controller.js'
 import IngredientsController from '../controllers/ingredients.controller.js'
-import AdminController from '../controllers/admin.controller.js'
 import MenuController from '../controllers/menu.controller.js'
+import logger from '../utils/logger.js'
 
 const router = Router()
 
-//POST ADD WAITER / TABLE / KITCHEN / ADMIN
-router.post('/register', AuthController.signUp)
+//POST ADD WAITER / KITCHEN / ADMIN
+router.post('/register', async (req, res) => {
+	const { username, password, name, role } = req.body
+
+	try {
+		const payload = await AuthController.signUp(username, password, name, role)
+
+		res.status(201).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: signUp ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //POST INGREDIENTES
-router.post('/ingredients', IngredientsController.addIngredient)
+router.post('/ingredients', async (req, res) => {
+	const { ingredientName, quantity } = req.body
+
+	try {
+		const payload = await IngredientsController.addIngredient(
+			ingredientName,
+			quantity,
+		)
+
+		res.status(201).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: addIngredients ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //GET INGREDIENTES
-router.get('/ingredients', IngredientsController.getIngredient)
+router.get('/ingredients', async (req, res) => {
+	try {
+		const payload = await IngredientsController.getIngredient()
+
+		res.status(200).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: getIngredients ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //DELETE INGREDIENTES
-router.delete('/ingredients/delete/:id', IngredientsController.removeIngredient)
+router.delete('/ingredients/delete/:id', async (req, res) => {
+	const { id } = req.params
+	try {
+		const payload = await IngredientsController.removeIngredient(id)
+
+		res.status(200).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: deleteIngredients ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //PATCH INGREDIENTES
-router.patch('/ingredients/update/:id', IngredientsController.updateIngredient)
+router.patch('/ingredients/update/:id', async (req, res) => {
+	const { id } = req.params
+	const { ingredientName, quantity } = req.body
+
+	try {
+		const payload = await IngredientsController.updateIngredient(
+			id,
+			ingredientName,
+			quantity,
+		)
+
+		res.status(200).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: updateIngredients ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //POST MENU
-router.post('/menu', AdminController.createMenu)
+router.post('/menu', async (req, res) => {
+	const data = req.body
+	try {
+		const payload = await AdminController.createMenu(data)
 
-//GET MENU
-router.get('/menu', MenuController.getMenu)
+		res.status(201).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: createMenu ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
+
+// GET MENU
+// {{host}}api/menu
 
 //PATCH MENU
-router.patch('/menu/update/:id', AdminController.updateMenu)
+router.patch('/menu/update/:id', async (req, res) => {
+	const { id } = req.params
+	const data = req.body
+
+	try {
+		const payload = await AdminController.updateMenu(id, data)
+
+		res.status(200).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: updateMenu ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //DELETE MENU
-router.delete('/menu/delete/:id', AdminController.removeMenu)
+router.delete('/menu/delete/:id', async (req, res) => {
+	const { id } = req.params
+
+	try {
+		const payload = await AdminController.removeMenu(id)
+		res.status(200).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: deleteMenu ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 //PATCH CLOSE DAY
-router.patch('/closeDay', AdminController.closeDay)
+router.patch('/closeDay', async (req, res) => {
+	try {
+		const payload = await AdminController.closeDay()
+
+		res.status(200).json(payload)
+	} catch (err) {
+		logger.error(`Error in admin.routes: closeDay ${err}`)
+		res.status(500).json({ message: 'Internal server error' })
+	}
+})
 
 export default router
